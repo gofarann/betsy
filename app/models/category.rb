3845ef49b9cf959646c1e3c3000834ef44ad02cd
Meighan Rasley <meighanr@gmail.com>
2015-12-09 13:55:36 -0800

    Add category dropdown to product#new view, removed has_many :categories_products from category and product rbs

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,4 @@
 class Category < ActiveRecord::Base
-  has_many :categories_products
   has_many :products, through: :categories_products
   belongs_to :user
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,5 @@
 class Product < ActiveRecord::Base
   belongs_to :user
-  has_many :categories_products
   has_many :reviews
   has_many :order_items
   has_many :categories, through: :categories_products
