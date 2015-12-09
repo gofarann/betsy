3056b3075f09323b60e5a16a45ef14bfc86ebebb
Annalee <annaleeherrera@gmail.com>
2015-12-09 14:14:01 -0800

    added logic in application, session, users controllers for user auth.  added create and new routes only for sessions path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,29 @@
 class SessionsController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
+
+  def new
+  end
+
+  def create
+    data = params[:session_data]
+    @user = User.find_by_email(data[:email_address])
+
+    if !@user.nil?
+      if @user.authenticate(data[:password])
+        session[:user_id] = @user.id
+        redirect_to root_path
+      else
+        flash.now[:error] = "Try Again"
+        render :new
+      end
+    else
+      flash[:error] = "Create a new user"
+      redirect_to new_user_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to new_session_path
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(strong_params)
+    if @user.save
+      redirect_to new_session_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def strong_params
+    params.require(:user).permit(:username, :email_address, :password, :password_confirmation)
+  end
 end
