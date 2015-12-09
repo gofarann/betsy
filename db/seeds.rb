298eed2785b59c9a1b37dc242e7f2d08f2cf59f9
Riley Spicer <riley.r.spicer@gmail.com>
2015-12-09 09:53:58 -0800

    adding more seedz

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,40 @@
 products = [
-  {name:"Emerald Sword", price: "200" , user_id: "1", stock: "2" , photo_url: "http://www.pixelpapercraft.com/files/1351191430408-small.png", description: "Let this delightful emerald sword help you thwack your way through the world." }, 
-  {name: "Pure Emerald Block", price: "600", user_id: "12", stock: "25", photo_url: "http://www.pixelpapercraft.com/files/1381466703842.png", description: "Use this lovely emerald block to build all the things."},
-  {name: "Fancy Necklace", price: "600", user_id: "14", stock: "22", photo_url: "http://www.faberge.com/images/thumbs/0006090_690.jpeg", description: "Look the best at your next fancy party."},
+  # Emerald
+  {name:"Emerald Sword", price: "20440" , user_id: "1", stock: "2" , photo_url: "http://www.pixelpapercraft.com/files/1351191430408-small.png", description: "Let this delightful emerald sword help you thwack your way through the world." }, 
+  {name: "Pure Emerald Block", price: "65500", user_id: "12", stock: "25", photo_url: "http://www.pixelpapercraft.com/files/1381466703842.png", description: "Use this lovely emerald block to build all the things."},
+  {name: "Fancy Necklace", price: "6050", user_id: "14", stock: "22", photo_url: "http://www.faberge.com/images/thumbs/0006090_690.jpeg", description: "Look the best at your next fancy party."},
   {name: "Gaudy Is For Lovers", price: "8000", user_id: "4", stock: "1", photo_url: "http://i.ebayimg.com/00/s/MzAwWDMwMA==/z/ZiYAAOSwnDZUAAOK/$_35.JPG?set_id=2", description: "Bedeck your hands with this one-of-a-kind glory."},
+  # Ruby
+  {name: "Ruby Sword", price: "7868", user_id: "2", stock: "5", photo_url: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcS7u0rM40peU5eoDCl7PCmyMpsg7c1iqco_dzvQn_1c_ah8L298hGXF9AU", description: "You won't even be able to see the blood."},
+  {name: "Blood Red Shovel", price: "666", user_id: "1", stock: "5", photo_url: "http://vignette1.wikia.nocookie.net/minecraftfanfictions/images/5/5e/Ruby_shovel.png/revision/latest?cb=20120803133632", description: "Bury the bodies of your enemies in style."},
+  {name: "One Ring to Rule Them All", price: "4254", user_id: "6", stock: "4", photo_url: "http://verdura.blob.core.windows.net/images/products/0578255bca9e4a539026537b0ec50fe7/large", description: "Just try to find a matching outfit for this one... "},
+  {name: "Ruby Ring", price: "542532", user_id: "6", stock: "55", photo_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRU5kK3fdZX3VXVQjR84Skk27lVwBjDV5NJnmmhSexK1CJqi8WhaQ", description: "It's a ring. With a ruby in it. "},
+  {name: "Raw Ruby Rock", price: "343", user_id: "15", stock: "3", photo_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTboRwcl_mhVPNT7hW03VNdENxn0bRXCto_dPuGzJNZfGTQGqUO", description: "I guess calling it a rock doesn't make you want to buy it."},
+  {name: "Funny Looking Ruby", price: "4353", user_id: "16", stock: "456", photo_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjOjxJma6TgDA-WOw2z1w0lZ0Yvw7epBR_Uy_IRjhfQoxQUypH", description: "It's synthetic, what did you expect?"},
+  {name: "Not Craisins", price: "453", user_id: "16", stock: "32", photo_url: "https://static.auctionmate.io/gr/uploads/images/255000-259999/257107/257107_1328612158.jpg", description: "Don't eat these rubies, they'll hurt your teeth."},
+  {name: "Pulsing Pendant", price: "4533", user_id: "11", stock: "34", photo_url: "http://www.raritetantique.com/img/products/Jewelry/Sets/Raw%20Ruby%20Set%20ring%20pendant/Vintage%20Raw%20Ruby%20Diamond%2014K%20Gold%20Ring%20Pendant%20Set%20(11).jpg", description: "Like gold fingers clenching your beating heart. It'll look great with your cheekbones."},
+  {name: "Ruby Necklace", price: "dsfs", user_id: "3", stock: "45", photo_url: "http://www.jared.com/images/products/1333/133361004_MV_ZM_JAR.jpg", description: "It'a necklace. You put it around your neck."},
+  {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
+  {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
+  # Diamond
+  {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
+  {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
+  {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
+  {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
+  {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
+  # Azurite
+  {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
+  {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
+  {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
+  {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
+  {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
+# Saphhire
+  {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
+  {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
+  {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
+  {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
+  {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
+# Quartz
   {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
   {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
   {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
@@ -11,12 +43,14 @@ products = [
   {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
   {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
   {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
+# Amethyst
   {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
   {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
   {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
   {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
   {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
   {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
+# Tourmaline
   {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
   {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
   {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
@@ -24,6 +58,11 @@ products = [
   {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
   {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
 
+# Turquoise
+  {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
+  {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
+  {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
+  {name: "", price: "", user_id: "", stock: "", photo_url: "", description: ""},
 ]
 
 
