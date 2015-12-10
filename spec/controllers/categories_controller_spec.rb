b6c70acdb2d1ebdcf907fd0aaf132bfc4f2c7494
Riley Spicer <riley.r.spicer@gmail.com>
2015-12-10 11:58:24 -0800

    category inde page test running

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe CategoriesController, type: :controller do
 
+  describe 'GET index' do
+    it 'is successful' do
+      get :index
+      expect(subject.response.status).to eq(200)
+    end
+  end
+
 end
