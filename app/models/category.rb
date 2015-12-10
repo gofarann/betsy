ce3b32763b6b03c75a0831f0d982e2a1ce112abd
Riley Spicer <riley.r.spicer@gmail.com>
2015-12-10 09:01:12 -0800

    added skeleton methods for category model, plsu details to new and show

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,23 @@
 class Category < ActiveRecord::Base
   has_many :products, through: :categories_products
   belongs_to :user
+
+  def index
+    @categories = Category.all
+  end
+  def show
+    @category = Category.find(params[:id])
+  end
+  def new
+    
+  end
+  def create
+    
+  end
+  def edit
+    
+  end
+  def update
+    
+  end
 end
