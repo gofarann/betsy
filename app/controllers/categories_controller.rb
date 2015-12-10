728f03dd93356ebe05c955f6f69ae392e0e9db55
Riley Spicer <riley.r.spicer@gmail.com>
2015-12-10 09:19:49 -0800

    fixed category controller methods, view getting there

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,22 @@
 class CategoriesController < ApplicationController
+before_action :require_login, only: [:new, :create, :edit, :update]
+
+  def index
+    @cat = Category.all
+  end
+  def show
+    @category = Category.find(params[:id])
+  end
+  def new
+    
+  end
+  def create
+    
+  end
+  def edit
+    
+  end
+  def update
+    
+  end
 end
