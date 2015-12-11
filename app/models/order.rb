d76db801682de1c76ee664c920ec7fcee49de89d
Riley Spicer <riley.r.spicer@gmail.com>
2015-12-10 20:23:38 -0800

    changed a few things so we can make orderitem and order at the same time

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,18 +3,30 @@ class Order < ActiveRecord::Base
   has_many :orderitems
   has_many :products
 
+  validate :existence_of_item
+  accepts_nested_attributes_for :orderitems
+
   validate :has_orderitem, on: :create
   validates :status, presence: true
-  validates :cc_name, presence: true
-  validates :email_address, presence: true
-  validates :mailing_address, presence: true
-  validates :cc_number, presence: true
-  validates :zip, presence: true
-  validates :cc_exp, presence: true
-  validates :cc_cvv, presence: true
+
+  # All of the following validations don't need to be true until the status is marked paid. Do we need them as validations, or can we just have the form have required field? (And if we want them as validations, maybe we should have them set as a custom validation that only runs if the status is marked as paid.)
+  
+  # validates :cc_name, presence: true
+  # validates :email_address, presence: true
+  # validates :mailing_address, presence: true
+  # validates :cc_number, presence: true
+  # validates :zip, presence: true
+  # validates :cc_exp, presence: true
+  # validates :cc_cvv, presence: true
 
   def has_orderitem
     !!self.orderitems
   end
 
+  private
+    def existence_of_item
+      unless self.orderitems.any?
+        errors[:orderitems] << "missing order item"
+      end
+    end
 end
--- app/models/orderitem.rb
+++ app/models/orderitem.rb
@@ -2,7 +2,14 @@ class Orderitem < ActiveRecord::Base
   belongs_to :order
   belongs_to :product
 
-  validates :order_id, presence: true
+  # validates :order_id, presence: true
   validates :product_id, presence: true
+
+  after_validation :set_order
+
+  protected
+    def set_order
+      self.order_id = Order.all.last.id
+    end
 
 end
