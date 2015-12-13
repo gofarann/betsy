8b22ff9ecd93346333e13db6a82a8b290d7bf01e
Riley Spicer <riley.r.spicer@gmail.com>
2015-12-12 18:00:21 -0800

    welcome index controller spec passing

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -3,7 +3,25 @@ require 'rails_helper'
 RSpec.describe WelcomeController, type: :controller do
   
   describe 'GET index' do
+    let(:good_hash) do {
+      username: "Burp",
+      email_address: "thing@thing.com",
+      password: "this",
+      password_confirmation: "this"
+    }
+    end
+    let(:second_hash) do {
+      username: "Ugh",
+      email_address: "athing@thing.com",
+      password: "this",
+      password_confirmation: "this"
+    }
+    end
     it 'is successful' do
+      user = User.create(good_hash)
+      user1 = User.create(second_hash)
+      cat = Category.create(name: "Cool Shit")
+      cat2 = Category.create(name: "Better Shit")
       get :index
       expect(subject).to render_template :index
     end
