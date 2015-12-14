03393c7fb9e9c5f0ac154dc2a883cc5916be1218
Meighan Rasley <meighanr@gmail.com>
2015-12-14 10:25:20 -0800

    Test for product review

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -106,6 +106,28 @@ RSpec.describe ProductsController, type: :controller do
     end
   end
 
+  describe "POST 'review'" do
+    let(:product) do
+      Product.create(name: "necklace", price: 10, user_id: 2, stock: 3)
+    end
+    let(:review_params) do
+    {
+      id: product.id,
+      review: {
+        rating: 4,
+        body: "so pretty!"
+      }
+    }
+    end
+
+    it "creates a new product review" do
+      post :review, review_params.merge(product_id: product.id)
+      last = Review.last
+      expect(last.rating).to eq 4
+    end
+  end
+
+
   describe "DELETE 'destroy'" do
     let(:product) do
       Product.create(name: "necklace", price: 10, user_id: 2, stock: 3)
