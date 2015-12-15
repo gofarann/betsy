979cf7b2377a46cfbeedb42d70c82a3d90659cf8
Meighan Rasley <meighanr@gmail.com>
2015-12-14 20:31:48 -0800

    Test for category new method

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe CategoriesController, type: :controller do
   let(:create_params) do {
     name: 'Category'
   }
-    
+
   end
 
   let (:show_item) {Category.create(create_params)}
@@ -16,6 +16,13 @@ RSpec.describe CategoriesController, type: :controller do
       get :show, id: show_item.id
       expect(response.status).to eq 200
     end
+
+  describe "GET 'new'" do
+    it "renders new view" do
+      get :new
+      expect(subject).to render_template :new if @current_user != nil
+    end
+  end
 
   it_behaves_like "a quartzy controller"
 end
