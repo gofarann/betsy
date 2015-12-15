b7069563714b5c4a0506e5836b53cec78c5fc2c5
Riley Spicer <riley.r.spicer@gmail.com>
2015-12-15 14:22:00 -0800

    won't get same category or user on front page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,9 +4,15 @@ class WelcomeController < ApplicationController
   def index
     x = rand(1..User.all.length)
     y = rand(1..User.all.length)
+    until y != x 
+      y = rand(1..User.all.length)
+    end
     @top_users = [User.find(x), User.find(y)]
     m = rand(1..Category.all.length)
     n = rand(1..Category.all.length)
+    until n != m
+      n = rand(1..Category.all.length)
+    end
     @top_categories = [Category.find(m), Category.find(n)]
   end
 end
