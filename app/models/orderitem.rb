d9c2b28788938ad6e7dd2c94f63b5d69c7e23451
Riley Spicer <riley.r.spicer@gmail.com>
2015-12-15 10:34:52 -0800

    orderitem#item_shipped working

--- app/models/orderitem.rb
+++ app/models/orderitem.rb
@@ -2,5 +2,10 @@ class Orderitem < ActiveRecord::Base
   belongs_to :order
   belongs_to :product
 
+  # decerements inventory of product
+  def item_shipped
+    self.product.stock -= 1
+    self.product.save
+  end
 
 end
