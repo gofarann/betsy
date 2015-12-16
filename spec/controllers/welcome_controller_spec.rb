07a21c4cc1f92cd9a9fb8dd18a750d0996cae030
Riley Spicer <riley.r.spicer@gmail.com>
2015-12-16 13:57:02 -0800

    welcome controller spec fixed

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -17,11 +17,53 @@ RSpec.describe WelcomeController, type: :controller do
       password_confirmation: "this"
     }
     end
+    let(:product_hash) do
+       {name: "Geometry Like Woahhh",
+        price: 5645,
+        stock: "1",
+        photo_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDeCvOq-lfd-xau5kCj_RZ5WOD1wldXJybYd9abKVYwZKaGAay",
+        description: "I drew this just for you.",
+        retired: false
+      }
+    end
+    let(:second_product) do
+       {name: "Another Product Thing",
+        price: 5645245,
+        stock: "2",
+        photo_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDeCvOq-lfd-xau5kCj_RZ5WOD1wldXJybYd9abKVYwZKaGAay",
+        description: "I drew this just for you.",
+        retired: false
+      }
+    end
+    let(:third_product) do
+       {name: "Another Thing",
+        price: 5645245,
+        stock: "2",
+        photo_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDeCvOq-lfd-xau5kCj_RZ5WOD1wldXJybYd9abKVYwZKaGAay",
+        description: "I drew this just for you.",
+        retired: false
+      }
+    end
+    let(:fourth_product) do
+       {name: "Fourth product",
+        price: 56445,
+        stock: "2",
+        photo_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDeCvOq-lfd-xau5kCj_RZ5WOD1wldXJybYd9abKVYwZKaGAay",
+        description: "I drew this just for you.",
+        retired: false
+      }
+    end
     it 'is successful' do
       user = User.create(user_hash)
       user1 = User.create(second_user_hash)
       cat = Category.create(name: "Cool Shit")
       cat2 = Category.create(name: "Better Shit")
+      p = Product.create(product_hash)
+      p2 = Product.create(second_product)
+      user.products << [p, p2]
+      user1.products << [Product.create(third_product), Product.create(fourth_product)]
+      cat.products << [p, p2]
+      cat2.products << [p, p2]
       get :index
       expect(subject).to render_template :index
     end
