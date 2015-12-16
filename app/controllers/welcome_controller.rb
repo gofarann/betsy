58e6af93999fed116c51651ec4b9f7d64b0a2147
Riley Spicer <riley.r.spicer@gmail.com>
2015-12-16 11:10:06 -0800

    front page won't show merchants with too few products for display

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,14 +3,20 @@ class WelcomeController < ApplicationController
     
   def index
     x = rand(1..User.all.length)
+    until User.find(x).products.length >= 2
+      x = rand(1..User.all.length)
+    end
     y = rand(1..User.all.length)
-    until y != x 
+    until y != x && User.find(y).products.length >= 2
       y = rand(1..User.all.length)
     end
     @top_users = [User.find(x), User.find(y)]
-    m = rand(1..Category.all.length)
+    m = rand(0..Category.all.length)
+    until Category.find(m).products.length >= 2
+      m = rand(1..Category.all.length)
+    end
     n = rand(1..Category.all.length)
-    until n != m
+    until n != m && Category.find(n).products.length >= 2
       n = rand(1..Category.all.length)
     end
     @top_categories = [Category.find(m), Category.find(n)]
