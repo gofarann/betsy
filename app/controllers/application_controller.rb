488f9e7b5c85487b48706d431a8ed5667a04a9ec
Meighan Rasley <meighanr@gmail.com>
2015-12-17 16:10:42 -0800

    Test that a guest or non-owner cannot retire a product

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,8 +14,8 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-     if session[:user_id] && !!User.exists?(session[:user_id])
-    @current_user ||= User.find(session[:user_id])
+    if session[:user_id] && !!User.exists?(session[:user_id])
+      @current_user ||= User.find(session[:user_id])
     end
   end
 
