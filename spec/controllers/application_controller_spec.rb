0c69191f018f30bc6e56a53a115dba130bc52b69
Riley Spicer <riley.r.spicer@gmail.com>
2015-12-17 16:45:34 -0800

    only_current_user spec stubbed

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -3,4 +3,10 @@ require 'support/app_controller'
 
 RSpec.describe ApplicationController, type: :controller do
 
+  describe "only_current_user" do
+    it "only allows current user access" do
+      
+    end
+  end
+
 end
