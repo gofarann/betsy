8ffa24e76292442bfedfad88783940e2c841d300
noglows <jessica.noglows@outlook.com>
2016-01-21 11:51:16 -0800

    Added box sizing logic

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -34,6 +34,75 @@ class OrdersController < ApplicationController
 
   def confirm
     @order = Order.find(session[:order_id])
+
+    # Handling box size logic
+    user_orderitems = {}
+    @order.orderitems.each do |oi|
+      if user_orderitems[oi.product.user.username].nil?
+        user_orderitems[oi.product.user.username] = [oi]
+      else
+        user_orderitems[oi.product.user.username] = user_orderitems[oi.product.user.username].push(oi)
+      end
+    end
+    boxes = []
+    user_orderitems.each do |store, order_items|
+      length = 0
+      width = 0
+      height = 0
+      weight = 0
+      order_items.each do |oi|
+        length += oi.product.length * oi.quantity
+        weight += oi.product.weight * oi.quantity
+        height += oi.product.height * oi.quantity
+        width += oi.product.width * oi.quantity
+      end
+      hash = {length: length, width: width, height: height}
+      max = hash.key(hash.values.max)
+      # NEED TO HANDLE CASE WHERE MULTIPLE BOXES ARE NEEDED
+
+      if hash[max] <= 91.4
+        if hash[max] <= 7.6
+          box = {length: 7.6, width: 7.6, height: 7.6}
+        elsif hash[max] <= 17.8
+          box = {length: 17.8, width: 17.8, height: 17.8}
+        elsif hash[max] <= 35.6
+          box = {length: 35.6, width: 35.6, height: 35.6}
+        elsif hash[max] <= 71.1
+          box = {length: 71.1, width: 71.1, height: 71.1}
+        elsif hash[max] <= 91.4
+          box = {length: 91.4, width: 91.4, height: 91.4}
+        end
+        boxes.push({weight: weight, size: box})
+      else
+        options = [7.6, 17.8, 35.6, 71.1, 91.4]
+        hash[max] -= 91.4
+        box = {length: 91.4, width: 91.4, height: 91.4}
+        boxes.push({weight: weight, size: box})
+        temp_box = []
+
+        while hash[max] >= 0
+
+          options.each do |option|
+            if hash[max] > 91.4
+              hash[max] -= 91.4
+              box = {length: 91.4, width: 91.4, height: 91.4}
+              temp_box.push(box)
+            elsif hash[max] <= option
+              hash[max] -= option
+              box = {length: option, width: option, height: option}
+              temp_box.push(box)
+            end
+          end
+          weight_each = weight/temp_box.length
+          temp_box.each do |t_box|
+            boxes.push({ weight: weight_each, size: t_box})
+          end
+        end
+
+      end
+
+    end
+    binding.pry
   end
 
   def cancel_as_guest
--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160120224335) do
+ActiveRecord::Schema.define(version: 20160120225540) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
