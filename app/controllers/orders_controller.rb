31a05f3eb7b4c46f88bba1aaa17f3448c326851b
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-01-22 14:05:13 -0800

    added error handling if HTTParty times out trying to reach shipping service API

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -45,12 +45,17 @@ class OrdersController < ApplicationController
     # @shipping_info = HTTParty.get("http://localhost:3000/rates?destination_address[country]=US&destination_address[state]=#{@order.state}&destination_address[city]=#{@order.city}&destination_address[zip]=#{@order.zip}&origin_address[country]=US&origin_address[state]=WA&origin_address[city]=Seattle&origin_address[zip]=98161&package[weight]=#{@box[:weight]}&package[length]=#{@box[:size]}&package[width]=#{@box[:size]}&package[height]=#{@box[:size]}&package[units]=metric",
     # headers: { 'Accept' => 'application/json' }, format: :json).parsed_response
 
+    begin
     @shipping_info = HTTParty.get("http://localhost:3000/rates",
        :headers => { 'Accept' => 'application/json' },
        :body => { :destination => {:country => "US", :state => "#{@order.state}", :city => "#{@order.city}", :zip => "#{@order.zip}" },
                  :origin => {:country => "US", :state => "WA", :city => "Seattle", :zip => "98161"},
                  :package => {:weight => @box[:weight], :length => @box[:size], :width => @box[:size], :height => @box[:size], :units => "metric"}})
 
+    rescue Net::ReadTimeout
+      @shipping_info = nil
+      flash[:notice] = "Our shipping service timed out! Please try again."
+    end
 
     #?destination_address[country]=US&destination_address[state]=#{@order.state}&destination_address[city]=#{@order.city}&destination_address[zip]=#{@order.zip}&origin_address[country]=US&origin_address[state]=WA&origin_address[city]=Seattle&origin_address[zip]=98161&package[weight]=#{@box[:weight]}&package[length]=#{@box[:size]}&package[width]=#{@box[:size]}&package[height]=#{@box[:size]}&package[units]=metric",
     #headers:  body: , format: :json).parsed_response
