baad35d8a590989086cdaf976b6f7639505dd485
noglows <jessica.noglows@outlook.com>
2016-01-22 10:41:53 -0800

    Updated API call working

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -45,20 +45,12 @@ class OrdersController < ApplicationController
     # @shipping_info = HTTParty.get("http://localhost:3000/rates?destination_address[country]=US&destination_address[state]=#{@order.state}&destination_address[city]=#{@order.city}&destination_address[zip]=#{@order.zip}&origin_address[country]=US&origin_address[state]=WA&origin_address[city]=Seattle&origin_address[zip]=98161&package[weight]=#{@box[:weight]}&package[length]=#{@box[:size]}&package[width]=#{@box[:size]}&package[height]=#{@box[:size]}&package[units]=metric",
     # headers: { 'Accept' => 'application/json' }, format: :json).parsed_response
 
-    # response = HTTParty.post("http://localhost:3000/rates",
-    #  :headers => { "Content-Type" => "application/json" },
-    #  #TODO: not hardcode in US destinations?
-    #  :body => {"destination" => { "country" => "US", "city" => "#{@order.city}", "state" => "#{@order.state}", "zip" => "#{@order.zip}" }}.to_json
-    #  )
 
     @shipping_info = HTTParty.get("http://localhost:3000/rates",
      :headers => { 'Accept' => 'application/json' },
-     :body => {"destination" => {"country" => "US", "state" => "#{@order.state}", "city" => "#{@order.city}", "zip" => "#{@order.zip}" },
-               "origin" => {"country" => "US", "state" => "WA", "city" => "Seattle", "zip" => "98161"},
-               "package" => {"weight" => @box[:weight], "length" => @box[:size], "width" => @box[:size], "height" => @box[:size], "units" => "metric"}}.to_json).parsed_response
-
-    #?destination_address[country]=US&destination_address[state]=#{@order.state}&destination_address[city]=#{@order.city}&destination_address[zip]=#{@order.zip}&origin_address[country]=US&origin_address[state]=WA&origin_address[city]=Seattle&origin_address[zip]=98161&package[weight]=#{@box[:weight]}&package[length]=#{@box[:size]}&package[width]=#{@box[:size]}&package[height]=#{@box[:size]}&package[units]=metric",
-    #headers:  body: , format: :json).parsed_response
+     :body => { :destination => {:country => "US", :state => "#{@order.state}", :city => "#{@order.city}", :zip => "#{@order.zip}" },
+               :origin => {:country => "US", :state => "WA", :city => "Seattle", :zip => "98161"},
+               :package => {:weight => @box[:weight], :length => @box[:size], :width => @box[:size], :height => @box[:size], :units => "metric"}})
   end
 
   def cancel_as_guest
