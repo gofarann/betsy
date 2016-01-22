80cd480e356e64506bc49d014584f43001687cdb
noglows <jessica.noglows@outlook.com>
2016-01-22 10:02:37 -0800

    Working on the box spec, still failing

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -44,6 +44,7 @@ class OrdersController < ApplicationController
 
     @shipping_info = HTTParty.get("http://localhost:3000/rates?destination_address[country]=US&destination_address[state]=#{@order.state}&destination_address[city]=#{@order.city}&destination_address[zip]=#{@order.zip}&origin_address[country]=US&origin_address[state]=WA&origin_address[city]=Seattle&origin_address[zip]=98161&package[weight]=#{@box[:weight]}&package[length]=#{@box[:size]}&package[width]=#{@box[:size]}&package[height]=#{@box[:size]}&package[units]=metric",
     headers: { 'Accept' => 'application/json' }, format: :json).parsed_response
+    #return @shipping_info
   end
 
   def cancel_as_guest
